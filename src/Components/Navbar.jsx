ed20973693429266f5276db6e2a7ccb97d70245d
T095T <pdeshpande540@gmail.com>
2025-09-09 17:42:12 +0530

    before-deployment-push

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
-const Navbar = ({ menuOpen, setMenuOpen }) => {
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
   }, [menuOpen]);
-
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="/" className="font-mono text-xl text-white">
-            T095T <span className="text-yellow-500">.tech</span>
+          <a href="#home" className="font-mono text-xl font-bold text-white">
+            {" "}
+            T095T<span className="text-yellow-500">.tech</span>{" "}
           </a>
+
           <div
-            className="w-7 h-5 relative cursor-pointer z-40 md:hidden ml-auto"
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
             onClick={() => setMenuOpen((prev) => !prev)}
           >
             &#9776;
@@ -22,31 +23,31 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
           <div className="hidden md:flex items-center space-x-8">
             <a
               href="#home"
-              className="text-gray-500 hover:text-white  transition-colors"
+              className="text-gray-300 hove:text-white transition-colors"
             >
-                {" "}
+              {" "}
               Home
             </a>
             <a
-              href="#projects"
-              className="text-gray-500 hover:text-white  transition-colors"
+              href="#about"
+              className="text-gray-300 hove:text-white transition-colors"
             >
-                {" "}
-              Projects
+              {" "}
+              About{" "}
             </a>
             <a
-              href="#about"
-              className="text-gray-500 hover:text-white  transition-colors"
+              href="#projects"
+              className="text-gray-300 hove:text-white transition-colors"
             >
-                {" "}
-              About
+              {" "}
+              Projects{" "}
             </a>
             <a
               href="#contact"
-              className="text-gray-500 hover:text-white  transition-colors"
+              className="text-gray-300 hove:text-white transition-colors"
             >
-                {" "}
-              Contact
+              {" "}
+              Contact{" "}
             </a>
           </div>
         </div>
@@ -54,5 +55,3 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
     </nav>
   );
 };
-
-export default Navbar;
