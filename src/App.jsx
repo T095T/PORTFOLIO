a37f329022395dafe624dbcd23a358e7ce7d02a9
T095T <pdeshpande540@gmail.com>
2025-09-09 13:12:48 +0530

    Navbar halfway

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
-import { LoadingScreen } from './Components/LoadingScreen'
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./Components/LoadingScreen";
 import "./index.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>
-      <LoadingScreen />
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    <div className={`min-h-screen trasition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
+
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
