15be451775bb2bf976381d5a4542e2f2c83f5284
T095T <pdeshpande540@gmail.com>
2025-09-14 21:15:58 +0530

    About,Home done

--- src/Components/sections/About.jsx
+++ src/Components/sections/About.jsx
@@ -9,7 +9,19 @@ export const About = () => {
     "Svelte",
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const backendSkills = ["Node.js", "Python", "MongoDB", "SQL"];
+
+  const machinelearningSkills = [
+    "TensorFlow",
+    "PyTorch",
+    "scikit-learn",
+    "Pandas",
+    "NumPy",
+  ];
+
+  const Platforms = ["Git", "Vercel"];
+
+  const Softwares = ["Cursor", "Figma"];
 
   return (
     <section
@@ -18,18 +30,25 @@ export const About = () => {
     >
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-yellow-400 bg-clip-text text-transparent text-center">
             {" "}
             About Me
           </h2>
 
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+              Yup! I’m a Developer. Big deal, right? But hold on — there’s more!
+              I’m not just writing code, I’m building intelligent solutions with
+              Data Science, Machine Learning, and Web Development. And guess
+              what? I love every bit of it.
+              <br />
+              <br />I enjoy exploring data, training models, and bringing ideas
+              to life on the web. Curiosity keeps me learning, and creativity
+              keeps me building. Whether it’s solving complex problems or
+              crafting sleek applications, I’m always up for the challenge......
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Frontend</h3>
                 <div className="flex flex-wrap gap-2">
@@ -61,6 +80,52 @@ export const About = () => {
                   ))}
                 </div>
               </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Machine Learning</h3>
+                <div className="flex flex-wrap gap-2">
+                  {machinelearningSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Platforms</h3>
+              <div className="flex flex-wrap gap-2">
+                {Platforms.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Softwares</h3>
+              <div className="flex flex-wrap gap-2">
+                {Softwares.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
             </div>
           </div>
 
@@ -69,12 +134,16 @@ export const About = () => {
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  <strong>
+                    {" "}
+                    B.E. in Artificial Intelligence and Data Science{" "}
+                  </strong>{" "}
+                  - AISSMS IOIT, Pune (2022-2025)
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  <strong>Relevant Coursework</strong>: Data Structures, Web
+                  Development, Cloud Computing,Data Science and Machine
+                  Learning.
                 </li>
               </ul>
             </div>
@@ -84,22 +153,12 @@ export const About = () => {
                 <div>
                   <h4 className="font-semibold">
                     {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
+                    Data Science Intern at Edutech Foundation (Feb 2024 - March
+                    2024 ){" "}
                   </h4>
                   <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
-                  </p>
-                </div>
-
-                <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Intern at DEF Startups (2019){" "}
-                  </h4>
-                  <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
+                    Developed and maintained interactive dashboards with Python
+                    and Power BI .
                   </p>
                 </div>
               </div>
